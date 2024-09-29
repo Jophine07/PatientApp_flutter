92f2437c7c9b58960408697cba8e91b3532c9e16
Jophine07 <jophinepaul@gmail.com>
2024-09-29 18:03:12 +0530

    Started

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+void main()
+{
+  runApp(Patient());
+}
+class Patient extends StatelessWidget {
+  const Patient({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
